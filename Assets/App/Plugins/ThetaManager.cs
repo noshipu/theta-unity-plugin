1fc788e3d46e41ace85e532e3936105b124087b0
能代 和哉 <noshipu@noudai-no-MacBook-Air.local>
2015-04-14 01:48:34 +0900

    fix iOS plugin first

---
+++ Assets/App/Plugins/ThetaManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class ThetaManager : MonoBehaviour {
+
+	// iOS側のコードを呼び出すための処理
+	[DllImport("__Internal")]
+	private static extern void Theta_Connect ();
+	[DllImport("__Internal")]
+	private static extern void Theta_DisConnect ();
+	[DllImport("__Internal")]
+	private static extern void Theta_Capture ();
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Connect()
+	{
+		Theta_Connect ();
+	}
+
+	public void DisConnect()
+	{
+		Theta_DisConnect ();
+	}
+
+	public void Capture()
+	{
+		Theta_Capture ();
+	}
+}
